fb403d470ececc440a55e69ca97a3a43eb9b8a26
bilalali2010 <bilalali0905@gmail.com>
2026-05-13 13:38:39 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-    serverExternalPackages: [
-      "tesseract.js",
-      "sharp"
-    ]
-  },
-
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("tesseract.js");
-      config.externals.push("sharp");
-    }
-
-    return config;
-  },
-
   output: "standalone"
 };
 
